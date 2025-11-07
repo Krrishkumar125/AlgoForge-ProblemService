e02f89636aa58d3d2128bce609ef11335764b9f6
Krrishkumar125 <krrishkumar2028@gmail.com>
2025-11-07 13:54:14 +0530

    Feat: Added mongoose db config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const {PORT,ENV} = require('./config/server.config');
+const {PORT,NODE_ENV} = require('./config/server.config');
 const apiRouter = require('./routes');
 const errorHandler = require('./utils/errorHandler');
+const connectToDB = require('./config/db.config');
 
 const app = express();
 
@@ -19,6 +20,8 @@ app.get('/ping', (req,res)=>{
 
 app.use(errorHandler);
 
-app.listen(PORT , ()=>{
-    console.log(`Server started at PORT ${PORT} in ${ENV} environment`);
+app.listen(PORT , async ()=>{
+    console.log(`Server started at PORT ${PORT} in ${NODE_ENV} environment`);
+    await connectToDB();
+    console.log("Successfully connected to DB");
 });
